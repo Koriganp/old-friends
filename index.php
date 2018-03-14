6d570a7078a16f1aaa3d1f003c2e1bb1cca30264
Korigan Payne <Koriganp@gmail.com>
2018-03-14 15:08:03 -0600

    added landing page, navbar, and styles

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+
+		<!-- Metadata -->
+
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width = device-width, user-scalable = no, initial-scale = 1.0, maximum-scale = 1.0, minimum-scale = 1.0"/>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+
+		<!-- Google Fonts -->
+
+		<link href="https://fonts.googleapis.com/css?family=Julius+Sans+One|Raleway" rel="stylesheet"/>
+
+		<!-- Bootstrap CSS -->
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
+
+		<!-- Font Awesome -->
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
+		<!-- My CSS -->
+
+		<link href="styles/main.css" rel="stylesheet"/>
+
+		<!-- jQuery, Popper.js, Bootstrap JS -->
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<title>Mock-up for password resetting</title>
+	</head>
+	<body>
+		<header>
+			<nav class="navbar fixed-top navbar-expand-md navbar-light site-nav">
+				<a class="navbar-brand brand-text" href="#"><img class="nav-icon space" src="images/KD.png"><strong> <span class="first-letter">K</span>origan Design</strong></a>
+				<button class="navbar-toggler position-toggle" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarToggler">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="text-dark my-2" href="https://korigandesign.com" target="_blank">Home </a>
+						</li>
+						<li class="nav-item">
+							<a class="text-dark my-2" href="https://korigandesign.com/services/index.php" target="_blank"> Services </a>
+						</li>
+						<li class="nav-item">
+							<a class="text-dark my-2" href="https://korigandesign.com/about/index.php" target="_blank"> About </a>
+						</li>
+						<li class="nav-item">
+							<a class="text-dark my-2" href="https://korigandesign.com/portfolio/index.php" target="_blank"> Portfolio </a>
+						</li>
+						<li class="nav-item">
+							<a class="text-dark my-2" href="https://korigandesign.com/contact/index.php" target="_blank"> Contact </a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col justify-content-center">
+						<div class="move-down">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<main>
+			<div class="jumbotron bg-white mt-5">
+				<h2 class="display-3">Hi James!!</h2>
+				<p class="lead font-weight-bold">So here's what I've got so far as the functionality goes. I don't have a database set up for it yet, so I can't tell if it's talking to the backend yet, but this is what the front end part would look like.</p>
+				<hr class="my-2">
+				<p class="nothing font-weight-bold">Press the button</p>
+				<h3>Korigan Payne</h3>
+				<p>Owner</p>
+				<div class="dropdown ml-5 pl-5">
+					<button class="btn btn-primary mr-5" type="button"><a class="" href="public_html/api/user/index.php" target="_blank">Login</a> </button>
+				</div>
+			</div>
+		</main>
+	</body>
+</html>
+
